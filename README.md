3b0f4dd215c6dd03d8eab8eaaedbf0f1e82c3962
f3ath <karapetov@gmail.com>
2020-07-28 19:10:38 -0700

    Attemp to use flutter create

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # skill_branch_flutter
 
-A new Flutter application.
+A new Flutter project.
 
 ## Getting Started
 
--- android/app/src/main/kotlin/ru/skillbranch/skill_branch_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package ru.skillbranch.skill_branch_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
