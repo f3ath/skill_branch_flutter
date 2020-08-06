f151c11589903641c73a09e6c6ca4242b17a85c8
f3ath <karapetov@gmail.com>
2020-08-05 19:55:41 -0700

    Restore String capitalize(String s)

--- lib/string_util.dart
+++ lib/string_util.dart
@@ -1,4 +1,6 @@
 extension CalitalizedString on String {
   String get capitalized =>
       this[0].toUpperCase() + this.substring(1).toLowerCase();
+
+  String capitalize(String s) => s.capitalized; // WTF
 }
